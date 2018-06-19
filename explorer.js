7e3c356eaa3ac35d8dbfdd8121d3008bb0c9be18
Andrey <admin@twister-vl.ru>
2018-06-20 02:19:27 +0300

    Wallet and transactions review

--- explorer.js
+++ explorer.js
@@ -4,11 +4,36 @@
  */
 
 const maxBlocksOnPage = 15;
+const RESPONSE_SUFFIX = '_RESP';
 
 var nodes = ["wss://bitcoen.io/blockchain/", "ws://localhost:6013", "ws://node1.bitcoen.io:6013", "ws://node2.bitcoen.io:6013", "ws://node3.bitcoen.io:6013", "ws://node4.bitcoen.io:6013"];
 var candy = null;
 var lastestBlocks = [];
 var parsers = {};
+
+var waitingMessages = [];
+
+var precision = 10000000000;
+
+/**
+ * Форматирует отображение числа согласно precision
+ * @param {Number} number
+ */
+function formatToken(number) {
+    if(precision === 1) {
+        return String(number);
+    }
+    var nulls = String(precision).replace(/[1-9]*/, '').length;
+    var result = String(Math.round(number));
+    var right = result.slice(-nulls);
+    if(nulls - right.length > 0) {
+        for (var i = 1; nulls - right.length; i++) {
+            right = '0' + right;
+        }
+    }
+    return (result.length <= nulls ? '0' : result.slice(0, -nulls)) + '.' + right;
+}
+
 
 $(document).ready(function () {
     $('#loadingModal').modal('show');
@@ -21,6 +46,7 @@ $(document).ready(function () {
     $('.returnButton').click(function () {
         $('#lastestBlocksPage').fadeIn();
         $('#blockDetailPage').hide();
+        $('#walletDetailPage').hide();
     });
 
     $('.searchForm').on('submit', function (event) {
@@ -30,7 +56,23 @@ $(document).ready(function () {
 
             loadBlockPreview(search);
         } else {
-            if(confirm('Search by hash may take a long time. Are you sure?')) {
+
+            getTransactionByHash(search, function (tx) {
+                if(tx) {
+                    loadBlockPreview(tx.block);
+                } else {
+                    getWalletInfo(search, function (wallet) {
+                        if(wallet) {
+                            //loadBlockPreview(wallet.block);
+                            loadWalletPreview(wallet.id);
+                        } else {
+                            alert('Tx hash or wallet not found');
+                        }
+                    });
+                }
+            });
+
+            /*if(confirm('Search by hash may take a long time. Are you sure?')) {
                 var blockId = 1;
 
                 function checkBlock() {
@@ -55,7 +97,7 @@ $(document).ready(function () {
                 }
 
                 checkBlock();
-            }
+            }*/
         }
     });
 
@@ -102,6 +144,19 @@ function startCandyConnection(nodes) {
         }, 1000);
 
     };
+
+    candy.onmessage = function (messageBody) {
+        for (var a in waitingMessages) {
+            if(waitingMessages.hasOwnProperty(a)) {
+                if(waitingMessages[a].id === messageBody.id) {
+                    if(waitingMessages[a].handle(messageBody)) {
+                        delete  waitingMessages[a];
+                    }
+                    return;
+                }
+            }
+        }
+    }
 }
 
 
@@ -132,6 +187,7 @@ function loadBlockPreview(index) {
         window.location.hash = index;
         var blockType = detectBlockType(rawBlock);
         $('#lastestBlocksPage').hide();
+        $('#walletDetailPage').hide();
         $('#blockDetailPage').fadeIn();
 
         $('.blockIndex').text(rawBlock.index);
@@ -154,6 +210,116 @@ function loadBlockPreview(index) {
 
 
     });
+}
+
+function loadWalletPreview(wallet) {
+
+    function openWallet(wallet) {
+        getWalletTransactions(wallet.id, function (txs) {
+
+            window.location.hash = 'BL_' + wallet.block;
+
+            $('#lastestBlocksPage').hide();
+            $('#blockDetailPage').hide();
+            $('#walletDetailPage').fadeIn();
+
+            $('.walletId').text('BL_' + wallet.block);
+            $('.walletBlock').text(wallet.block);
+            $('.fullAddress').text(wallet.id);
+            $('.public').text(wallet.keysPair.public);
+            $('.totalTx').text(txs.income.length + txs.outcome.length);
+            $('.walletBalance').text(formatToken(wallet.balance));
+
+            var transactions = [];
+
+            for (var a in txs.income) {
+                if(txs.income.hasOwnProperty(a)) {
+                    transactions.push(txs.income[a]);
+                }
+            }
+
+            for (a in txs.outcome) {
+                if(txs.outcome.hasOwnProperty(a)) {
+                    transactions.push(txs.outcome[a]);
+                }
+            }
+
+            transactions = transactions.sort(function (b1, b2) {
+                return (b2.timestamp - b1.timestamp)
+            });
+
+            var html = "<table class='transactionsTable' style=\"width:100%\">\n" +
+                "  <tr>\n" +
+                "    <th>Block #</th>\n" +
+                "    <th>Timestamp</th>\n" +
+                "    <th>Activate from</th>\n" +
+                "    <th>Operation</th> \n" +
+                "    <th>Amount</th>\n" +
+                "    <th>From</th>\n" +
+                "    <th>To</th>\n" +
+                "  </tr>\n";
+
+
+            for (a in transactions) {
+                var tx = transactions[a];
+
+                if(candy.blockHeight - tx.block < 20) {
+                    html += "<tr style='background: #f9ffb1'>";
+                    html += "<td title='Not active' title='Unaccepted'> 🕐 <a href='#' class='blockHref' >" + tx.block + "</a></td>";
+                } else {
+                    html += "<tr>";
+                    html += "<td> <a href='#' class='blockHref' >" + tx.block + "</a></td>";
+                }
+
+
+                html += "<td>" + moment(Number(tx.timestamp)).format('LLLL') + "</td>";
+
+                if(moment().utc().valueOf() < Number(tx.from_timestamp)) {
+                    html += "<td style='color: #959500'> 🕐 " + moment(Number(tx.from_timestamp)).format('LLLL') + "</td>";
+                }
+                else {
+                    html += "<td>" + moment(Number(tx.from_timestamp)).format('LLLL') + "</td>";
+                }
+
+
+                if(tx.from_address == tx.to_address) {
+                    html += "<td style='font-size: 60px; color: red; padding-bottom: 10px'><img src='http://www.kolobok.us/smiles/standart/dance2.gif'></td>";
+                } else if((tx.from_address) === (wallet.id)) { //outcome
+                    html += "<td style='font-size: 60px; color: red; padding-bottom: 10px'>-</td>";
+                } else {
+                    html += "<td style='font-size: 60px; color: green; padding-bottom: 10px'>+</td>";
+                }
+
+                html += "<td>" + formatToken(tx.amount) + "</td>";
+
+                html += "<td><a href='#' data-wallet='" + tx.from_address + "' class='walletHref'>" + tx.from_address.substr(0, 10) + "...</a></td>";
+                html += "<td><a href='#' data-wallet='" + tx.to_address + "' class='walletHref'>" + tx.to_address.substr(0, 10) + "...</a></td>";
+
+
+                html += "</tr>";
+            }
+
+            html += "</table>";
+
+
+            $('.txs').html(html);
+
+            $('.walletHref').click(function () {
+                loadWalletPreview($(this).data('wallet'));
+            });
+
+        });
+    }
+
+    if(typeof wallet === 'string') {
+        getWalletInfo(wallet, function (wallet) {
+            if(wallet) {
+                openWallet(wallet);
+            }
+        })
+    } else {
+        openWallet(wallet);
+    }
 }
 
 
@@ -202,4 +368,68 @@ function updateLatestBlocks() {
             });
         }
     }
+}
+
+/**
+ * Get transactions for full wallet address
+ * @param id
+ * @param cb
+ */
+function getWalletTransactions(id, cb) {
+    waitingMessages.push({
+        id: 'getWalletTransactions' + RESPONSE_SUFFIX, handle: function (message) {
+            message = message.data;
+            if(message.wallet === id) {
+                cb(message.txs);
+                return true;
+            }
+
+            return false;
+        }
+    });
+
+    candy.broadcastMessage({id: id}, 'getWalletTransactions', candy.recieverAddress, candy.recieverAddress);
+}
+
+/**
+ * Get transaction by hash
+ * @param id
+ * @param cb
+ */
+function getTransactionByHash(id, cb) {
+    waitingMessages.push({
+        id: 'getTransactionByHash' + RESPONSE_SUFFIX, handle: function (message) {
+            message = message.data;
+            if(message.hash === id) {
+                cb(message.txs);
+                return true;
+            }
+
+            return false;
+        }
+    });
+
+    candy.broadcastMessage({id: id}, 'getTransactionByHash', candy.recieverAddress, candy.recieverAddress);
+}
+
+
+/**
+ * Get wallet info by address
+ * @param id
+ * @param cb
+ */
+function getWalletInfo(id, cb) {
+    waitingMessages.push({
+        id: 'getWalletInfo' + RESPONSE_SUFFIX, handle: function (message) {
+            message = message.data;
+            if(message.wallet === id) {
+                cb(message.walletInfo);
+                return true;
+            }
+
+            return false;
+        }
+    });
+
+    candy.broadcastMessage({id: id}, 'getWalletInfo', candy.recieverAddress, candy.recieverAddress);
 }
